814501002e93f4c84909530aa8d366d7a0e335dc
minforever557-netizen <minforever557@gmail.com>
2026-02-06 15:42:21 +0700

    Add Firebase configuration and initialization

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+ // ================= FIREBASE CONFIG =================
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCsUhBsWjZfQuJo5lgprYt3xikQUyf8iiw",
+
+    authDomain: "sale-support-system.firebaseapp.com",
+
+    projectId: "sale-support-system",
+
+    storageBucket: "sale-support-system.appspot.com",
+
+    messagingSenderId: "640337786680",
+
+    appId: "1:640337786680:web:fd80699ac291b8d7a7b3cd"
+
+};
+
+// ================= INIT =================
+
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app); 

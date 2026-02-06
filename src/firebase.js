5509d2d7f75792bc3f82103b7ee5219e560f5715
minforever557-netizen <minforever557@gmail.com>
2026-02-06 15:42:55 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,16 @@
- // ================= FIREBASE CONFIG =================
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyCsUhBsWjZfQuJo5lgprYt3xikQUyf8iiw",
-
     authDomain: "sale-support-system.firebaseapp.com",
-
     projectId: "sale-support-system",
-
     storageBucket: "sale-support-system.appspot.com",
-
     messagingSenderId: "640337786680",
-
     appId: "1:640337786680:web:fd80699ac291b8d7a7b3cd"
-
 };
 
-// ================= INIT =================
-
-export const app = initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-export const db = getFirestore(app); 
+export const db = getFirestore(app);

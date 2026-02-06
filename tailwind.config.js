8b78e2c559d4bdd19ad3c6f1fc012e3f1ca474f0
minforever557-netizen <minforever557@gmail.com>
2026-02-06 15:41:34 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: { green: "#16a34a", orange: "#f97316" }
+      }
+    },
+  },
+  plugins: [],
+}

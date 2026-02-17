4fc46d2d1679dce59d30c883458bb9ea0664f9ad
minforever557-netizen <minforever557@gmail.com>
2026-02-17 10:30:12 +0700

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAa2uSD_tjNqYE2eXnZcn75h_jAVscDG-c",
+  authDomain: "salesupportsystemapp.firebaseapp.com",
+  projectId: "salesupportsystemapp",
+  storageBucket: "salesupportsystemapp.firebasestorage.app",
+  messagingSenderId: "840890441207",
+  appId: "1:840890441207:web:f3a5076d46e963a90de2f2"
+};
+
+export const app = initializeApp(firebaseConfig);
